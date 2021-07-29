a3c4d21414cc75d1079a028c1d600602b57d9c17
MehdiZed24 <mehdiziani1989@gmail.com>
2021-07-29 14:30:09 +0200

    Rajout de la feuille css, vehicule OK

--- views/vehicule.php
+++ views/vehicule.php
@@ -5,9 +5,9 @@ require_once('../src/controllers/vehicule.controller.php');
 require_once('../src/models/vehicule.model.php');
 
 
-if (!empty ($_POST['titre']) && !empty($_POST['marque']) && !empty($_POST['modele']) && !empty($_POST['prix']) && !empty($_POST['description']) && !empty($_FILES['photo'])) {
+if (!empty($_POST['titre']) && !empty($_POST['marque']) && !empty($_POST['modele']) && !empty($_POST['prix']) && !empty($_POST['description']) && !empty($_FILES['photo'])) {
   if (isset($_FILES['photo'])) {
-//RAJOUT DE $_GET !empty($_GET['titre_agence']) &&
+    //RAJOUT DE $_GET !empty($_GET['titre_agence']) &&
 
 
     $tmpName = $_FILES['photo']['tmp_name'];
@@ -27,7 +27,7 @@ if (!empty ($_POST['titre']) && !empty($_POST['marque']) && !empty($_POST['model
 
 $result = new AgenceModel;
 // var_dump($result);
-$results=$result->read();
+$results = $result->read();
 // echo '<pre>';
 // var_dump($results);
 // echo '</pre>';
@@ -46,51 +46,82 @@ $resultUn = $resultOne->read();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/styles.css">
   <title>Gestion des véhicules</title>
 </head>
 
 <body>
+  <?php include_once('./template/header.php'); ?>
   <h1>Gestion des véhicules</h1>
-<form method="get" action="vehicule.php">
-<select name="id_agence">
+
+
+
+  <form method="get" action="vehicule.php">
+    <select name="id_agence">
       <option>--Selection Agence--</option>
-      <?php foreach ($results as $values):?>
-      <option value="<?=$values['id_agence'];?>"><?= $values["titre_agence"];?></option>
-      <?php endforeach;?>
+      <?php foreach ($results as $values) : ?>
+        <option value="<?= $values['id_agence']; ?>"><?= $values["titre_agence"]; ?></option>
+      <?php endforeach; ?>
     </select>
     <button>Rediriger vers l'agence</button>
-</form>
-
-<br>
-<form action="vehicule.php?id_agence=<?=$_GET['id_agence'] ?>" method="post" enctype="multipart/form-data">
-    <label for="titre">Titre</label>
-    <input type="text" id="titre" name="titre" placeholder="Titre de l'annonce">
-
-    <label for="marque">Marque</label>
-    <input type="text" id="marque" name="marque" placeholder="Marque">
-
-    <label for="modele">Modele</label>
-    <input type="text" id="modele" name="modele" placeholder="Modele">
-
-    <label for="prix">Prix</label>
-    <input type="text" id="prix" name="prix" placeholder="Prix journalier">
-
-
-    <label for="description">Description</label>
-    <input type="text" id="description" name="description">
-    <br>
-
-    <label for="photo">Photo</label>
-    <input type="file" id="photo" name="photo">
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-<!--FAIRE TABLEAU -->
-    <button>Enregistrer</button>
-
   </form>
+
+
+
+
+  <form action="vehicule.php?id_agence=<?= $_GET['id_agence'] ?>" method="post" enctype="multipart/form-data">
+    <div class="champ"> <label for="titre">Titre</label>
+      <input type="text" id="titre" name="titre" placeholder="Titre de l'annonce">
+    </div>
+    <div class="champ"><label for="marque">Marque</label>
+      <input type="text" id="marque" name="marque" placeholder="Marque">
+    </div>
+    <div class="champ"> <label for="modele">Modele</label>
+      <input type="text" id="modele" name="modele" placeholder="Modele">
+    </div>
+
+    <div class="champ">
+      <label for="description">Description</label>
+      <input type="text" id="description" name="description">
+    </div>
+    <div class="champ"> <label for="photo">Photo</label>
+      <input type="file" id="photo" name="photo">
+    </div>
+
+    <div class="champ"> <label for="prix">Prix</label>
+      <input type="text" id="prix" name="prix" placeholder="Prix journalier">
+    </div>
+    <button>Enregistrer</button>
+  </form>
+  <table>
+    <thead>
+      <tr>
+        <th>Agence</th>
+        <th>Titre Véhicule</th>
+        <th>Marque</th>
+        <th>Modele</th>
+        <th>Description</th>
+        <th>Photo</th>
+        <th>Prix / Jour</th>
+
+      </tr>
+    </thead>
+    <?php foreach ($resultUn as $values) : ?>
+
+      <tr>
+        <td><?= $values['id_agence'] ?></td>
+        <td><?= $values['titre_vehicule'] ?></td>
+        <td><?= $values['marque'] ?></td>
+        <td><?= $values['modele'] ?></td>
+        <td><?= $values['description'] ?></td>
+        <td><?= $values['photo'] ?></td>
+        <td><?= $values['prix_journalier'] ?></td>
+      </tr>
+
+    <?php endforeach; ?>
+  </table>
+  
+  <?php include_once('./template/footer.php'); ?>
 </body>
 
 </html>
